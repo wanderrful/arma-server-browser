5a12b26fd7584f62dd6fb0495eb4d702eb55117a
Wanderrful <galois@mentlegen.com>
2017-10-27 16:13:01 +0900

    Stringifying front-end output

--- build/index.js
+++ build/index.js
@@ -17,7 +17,7 @@ function main() {
         res.render("index", {
             title: "hello",
             message: "world!",
-            server_list: lib.server_data
+            server_list: JSON.stringify(lib.server_data, null, 4)
         }, (err, html) => {
             if (err) {
                 lib.fn_log("ERROR: failed to render index\n" + err.stack);
